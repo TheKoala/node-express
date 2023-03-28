2477f9acea9dc635bdd3ceeb91412502eef23e2d
Felipe Lima <felipelima8556@gmail.com>
2023-03-28 11:32:09 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,20 @@
+const http = require("http");
+const port = 3000;
+
+const rotas = {
+  "/": "Hello World",
+  "/livros": "Entrei na pag de livros",
+  "/autores": "Listagem de autores",
+  "/editora": "Pag de editora",
+  "/sobre": "Info sobre o projeto",
+};
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  //res.end("Hello World");
+  res.end(rotas[req.url]);
+});
+
+server.listen(port, () => {
+  console.log(`Servidor escutando em http://localhost:${port}`);
+});
