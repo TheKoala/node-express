0f7d059cee978e5b8598c9d59af01e4b6ec6d3fc
Felipe Lima <felipelima8556@gmail.com>
2023-04-25 13:53:32 -0300

    gitignore update

--- src/config/dbConnect.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-mongoose.connect(
-  "mongodb+srv://felipelima:<password>@koala0.iinawhz.mongodb.net/alura-node"
-); 
-
-let db = mongoose.connection;
-
-export default db;
