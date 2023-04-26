b97d812be99b8410f1c6a94088b348dcffd349ef
Felipe Lima <felipelima8556@gmail.com>
2023-04-26 12:56:47 -0300

    save a livro on db

--- src/controllers/livroController.js
+++ src/controllers/livroController.js
@@ -1,7 +1,6 @@
 import livros from "../models/Livro.js";
 
 class LivroController {
-
   static listarLivros = (req, res) => {
     livros
       .find()
@@ -10,7 +9,22 @@ class LivroController {
       })
       .catch((erro) => console(erro));
   };
-  
+
+  static cadastrarLivro = (req, res) => {
+    let livro = new livros(req.body);
+    livro
+      .save()
+      .then(() => {
+        res.status(201).send(livro.toJSON());
+      })
+      .catch((erro) => {
+        if (erro) {
+          res
+            .status(500)
+            .send({ message: `${erro.message} - falha ao cadastrar livro` });
+        }
+      });
+  };
 }
 
 export default LivroController;
