a0d1554a54088350738fad2649c340dd445a0eb8
Felipe Lima <felipelima8556@gmail.com>
2023-04-28 11:01:18 -0300

    create an autor schema, controller and route

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,13 @@
 import express from "express";
 import livro from "./livroRoute.js";
+import autor from "./autorRoute.js";
 
 const routes = (app) => {
   app.route("/").get((req, res) => {
     res.status(200).send("Hello Express");
   });
 
-  app.use(express.json(), livro);
+  app.use(express.json(), livro, autor);
 };
 
 export default routes;
